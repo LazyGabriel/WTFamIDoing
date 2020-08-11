553327eb1328a7377228087d8c66a4d67897a5e2
LazyGabriel <ivanlight200@gmail.com>
2020-08-12 00:08:03 +0300

    Updates

---
+++ precache-manifest.a8e2477d77ff0351ae3ddc8d6d5a64fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdda8b63891b6df632f5c34f287a43ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64055b68c760446bb553",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "75d3cf695487a35f7967",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "64055b68c760446bb553",
+    "url": "./static/js/2.862318f6.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.862318f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d3cf695487a35f7967",
+    "url": "./static/js/main.b5c033ad.chunk.js"
+  },
+  {
+    "revision": "094b4cead0d7f92d7d3b",
+    "url": "./static/js/runtime-main.e2eecd6e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
